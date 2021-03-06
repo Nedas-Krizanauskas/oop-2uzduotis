aed32868f132c3af62d95a87d753c0dc10c71b12
Nedas-Krizanauskas <78591813+Nedas-Krizanauskas@users.noreply.github.com>
2021-03-06 22:03:18 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,19 +1,29 @@
 #include <iostream>
 #include "funkcijos.h" // programos funkcijos, studentu info
 
-void nuskaityti(vector<Studentas> *studentai, bool *vidurkis);
-void spausdintiRezultatus(vector<Studentas> studentai, bool *vidurkis);
+void nuskaityti(vector<Studentas> *studentai, bool *vidurkis, bool *isFailo);
+void nuskaitytiIsFailo(vector<Studentas> *studentai, bool*isFailo);
+void spausdintiRezultatus(vector<Studentas> studentai, bool *vidurkis, bool* isFailo);
 
 int main() {
-  bool vidurkis = true;
+  bool vidurkis = true, isFailo = false;
 
   vector<Studentas> studentai;
-  nuskaityti(&studentai, &vidurkis);
-  spausdintiRezultatus(studentai, &vidurkis);
+  nuskaityti(&studentai, &vidurkis, &isFailo);
+  spausdintiRezultatus(studentai, &vidurkis, &isFailo);
 }
 
-void nuskaityti(vector<Studentas> *studentai, bool *vidurkis) {
+void nuskaityti(vector<Studentas> *studentai, bool *vidurkis, bool *isFailo) {
   string s_temp;
+  cout << "Ar nuskaityti duomenis is failo?" << endl;
+  cin >> s_temp;
+
+  // jeip ats = taip, tai skaitom duomenis is failo
+  if(gautiAtsakyma(s_temp)) {
+    nuskaitytiIsFailo(studentai, isFailo);
+    return;
+  }
+
   int i_temp, dydis;
 
   // Gauna studentu skaiciu
@@ -76,14 +86,13 @@ void nuskaityti(vector<Studentas> *studentai, bool *vidurkis) {
     dydis = stud.pazymiai.size();
 
     if(vidurkis) {
-      stud.vidurkis = apvalinti((accumulate(stud.pazymiai.begin(),  stud.pazymiai.end(), 0.0) / stud.pazymiai.size()) * 0.4);
+      stud.vidurkis = skaiciuotiVidurki(stud);
     } else {
       // maz - didz sort'as
       sort(stud.pazymiai.begin(), stud.pazymiai.end());
 
       // medianos skaiciavimas, tikrinama ar lyginis skaicius pazymiu ar ne
-      stud.mediana = stud.pazymiai.size() % 2 == 0 ? 
-      (stud.pazymiai[dydis / 2 - 1] + stud.pazymiai[dydis / 2]) / 2 : stud.pazymiai[dydis/ 2];
+      stud.mediana = skaiciuotiMediana(stud);
     }
 
     studentai -> push_back(stud);
@@ -91,14 +100,96 @@ void nuskaityti(vector<Studentas> *studentai, bool *vidurkis) {
   }
 }
 
-void spausdintiRezultatus(vector<Studentas> studentai, bool *vidurkis) {
+void nuskaitytiIsFailo(vector<Studentas> *studentai, bool* isFailo) {
+  int pazymiuSkaicius = 0, dabarSk = 1, paz;
+  ifstream kursiokai;
+  kursiokai.open("kursiokai.txt");
+
+  string duom;
+  Studentas stud;
+
+  bool loop = true; // false jei klaida duomenyse
+
+  int rezultatas = 0;
+  // rezultatas = 0, tai dar vis skaitoma pirma eilute
+  // rezultatas = 1, skaitomas vardas
+  // rezultatas = 2, skaitoma pavarde
+  // rezultatas = 3, skaitomas paz.
+  // rezultats = 4, skaitomas egz
+  loop:while(kursiokai >> duom && loop) {
+    switch(rezultatas) {
+      case 0: {
+        if(startsWith(duom, "ND")) {
+          pazymiuSkaicius++;
+        } else if(startsWith(duom, "Egz")) {
+          rezultatas++;
+        }
+        break;
+      }
+      case 1: {
+        stud.vardas = duom;
+        rezultatas++;
+        break;
+      }
+      case 2: {
+        stud.pavarde = duom;
+        rezultatas++;
+        break;
+      }
+      case 3: {
+        if(dabarSk >= pazymiuSkaicius) {
+          rezultatas++;
+          dabarSk = 1;
+          break;
+        }
+
+        dabarSk++;
+        paz = parse(duom);
+        if(paz == -2 || arPazymys(duom) == -2) {
+          loop = false;
+          cout << "Ivestas neteisingas pazymys studento: " << stud.vardas << " " << stud.pavarde << endl;
+          cout << "Pazymio ivestis: " << duom << ", pazymio nr.: " << dabarSk << endl;
+          break;
+        }
+
+        stud.pazymiai.push_back(paz);
+        break;
+      }
+      case 4: {
+        stud.egzaminas = parse(duom);
+        
+        // Medianai sort'ina
+        sort(stud.pazymiai.begin(), stud.pazymiai.end());
+        stud.vidurkis = skaiciuotiVidurki(stud);
+        stud.mediana = skaiciuotiMediana(stud);
+
+        studentai -> push_back(stud);
+        stud = {};
+
+        rezultatas = 1;
+        break;
+      }
+    }
+  }
+
+}
+
+void spausdintiRezultatus(vector<Studentas> studentai, bool *vidurkis, bool *isFailo) {
+
+  cout << std::fixed;
+  cout << std::setprecision(2);
+
   cout << left
 	<< setw(15) << "Vardas"
 	<< setw(15) << "Pavarde";
 
-  cout << setw(15) << 
-  (vidurkis ?  "Galutinis (Vid.)" : "Galutinis (Med.)" ) << endl;
-		
+  if(!isFailo) {
+    cout << setw(15) << 
+    (vidurkis ?  "Galutinis (Vid.)" : "Galutinis (Med.)" ) << endl;
+	} else {
+    cout << setw(16) << "Galutinis (Vid.)" ;
+    cout << setw(16) << "Galutinis (Med.)" << endl;
+  }
   cout << "------------------------------------------------------------" << endl;
 
   Studentas stud;
@@ -108,6 +199,11 @@ void spausdintiRezultatus(vector<Studentas> studentai, bool *vidurkis) {
 			<< setw(15) << stud.vardas
 			<< setw(15) << stud.pavarde;
 
-      cout << setw(15) << (stud.egzaminas + (vidurkis ? stud.vidurkis : stud.mediana)) << endl;
+      if(!isFailo) {
+        cout << setw(15) << (stud.egzaminas + (vidurkis ? stud.vidurkis : stud.mediana)) << endl;
+      } else {
+        cout << setw(16) << (stud.egzaminas + stud.vidurkis);
+        cout << setw(16) << stud.mediana << endl;
+      }
   }
 }
